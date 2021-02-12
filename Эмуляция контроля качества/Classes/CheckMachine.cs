386d5ff43a2ce8b7bb31a2386173f9e9902cab6c
Alexandr Ershov <ershov.sasha2013@gmail.com>
2021-02-12 20:31:37 +0700

    Revert "Revert "Создание деталей""
    This reverts commit ed69898847123c74389e81ee1feafe86902060aa.

---
+++ Эмуляция контроля качества/Classes/CheckMachine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Эмуляция_контроля_качества.Classes
+{
+    class CheckMachine : ICheckMachine
+    {
+        public void Check()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Transit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Utilize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Эмуляция контроля качества/Classes/Nail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Эмуляция_контроля_качества.Classes
+{
+    class Nail : IDetail
+    {
+        public int Height { get; }
+        public int Width { get; }
+        public int Length { get; }
+    }
+}
