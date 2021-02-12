bf4e2bc1e2d2392437a885a392b9fb9041ba884e
Alexandr Ershov <ershov.sasha2013@gmail.com>
2021-02-12 19:36:32 +0700

    Добавьте файлы проекта.

---
+++ Эмуляция контроля качества/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Эмуляция_контроля_качества
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
